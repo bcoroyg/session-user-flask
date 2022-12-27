472803eb6982c6576bb726ad030ad8a2ce4c29eb
Emilio Coroy <becgonzalez25@gmail.com>
2022-12-27 08:02:14 -0600

    consultas a la base de datos

--- database.py
+++ database.py
@@ -14,3 +14,23 @@ config = {
 
 def connectdb():
     return pymysql.connect(**config)
+
+
+def create_user(email, password):
+    connect = connectdb()
+    with connect.cursor() as cursor:
+        cursor.execute("INSERT INTO user(email, password) VALUES (%s, %s)", (email, password))
+        connect.commit()
+        connect.close()
+        
+def get_user_by_email(email):
+    connect = connectdb()
+    user = None
+    with connect.cursor() as cursor:
+        cursor.execute("SELECT email, password FROM user WHERE email=%s", (email))
+        user = cursor.fetchone()
+        connect.close()
+        return user
+
+#if __name__ == '__main__':
+    #print(get_user_by_email("email@correo.com"))
